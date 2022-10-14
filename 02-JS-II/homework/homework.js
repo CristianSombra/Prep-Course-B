9c3e398e0844ebb18547c64cf64335a158612e43
CristianSombra <cristiansombra87@gmail.com>
2022-10-14 10:30:54 -0300

    Exercises finished 14/10/22

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -178,51 +178,92 @@ function devolverString(str) {
 
   }
   
-  function esPositivo(numero) {
+function esPositivo(numero) {
     //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
     //Si el número es positivo, devolver ---> "Es positivo"
     //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
     // Tu código:
-  }
+    if (numero === 0) {
+        return false;
+    }
+
+    else if (numero > 0) {
+        return "Es positivo";
+    }
+
+    else if (numero < 0) {
+        return "Es negativo";
+    }
+
+}
+
+
+  
   
   function agregarSimboloExclamacion(str) {
     // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
     // Ejemplo: "hello world" pasaría a ser "hello world!"
     // Tu código:
+      var nuevaString = str + "!";
+
+      return nuevaString;
   }
   
   function combinarNombres(nombre, apellido) {
     // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
     // Ejemplo: "Soy", "Henry" -> "Soy Henry"
     // Tu código:
+      var combinar = nombre + " " + apellido;
+
+      return combinar;
   }
   
   function obtenerSaludo(nombre) {
     // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
     // "Martin" -> "Hola Martin!"
-    // Tu código:
+      // Tu código:
+      var saludo = "Hola " + nombre + "!";
+
+      return saludo;
+
   }
   
   function obtenerAreaRectangulo(alto, ancho) {
     // Retornar el area de un rectángulo teniendo su altura y ancho
     // Tu código:
+      var resultado = alto * ancho;
+
+      return resultado;
+
   }
   
   function retornarPerimetro(lado) {
     //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
     //Escribe tu código aquí
+      var perimetro = lado * 4;
+
+      return perimetro;
+
   }
   
   function areaDelTriangulo(base, altura) {
     //Desarrolle una función que calcule el área de un triángulo.
     //Escribe tu código aquí
+      var area = (base * altura) / 2;
+
+      return area;
+
   }
   
   function deEuroAdolar(euro) {
     //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
     //como parámetro un número de euros y calcule el cambio en dólares.
     //Escribe tu código aquí
+      var calculo = euro * 1.20;
+
+      return calculo;
+
   }
   
   // No modificar nada debajo de esta línea
